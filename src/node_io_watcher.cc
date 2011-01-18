888b1a7d5b107d7ed05eeb719eada83e1a0dc52b
Ryan Dahl <ry@tinyclouds.org>
2011-01-18 12:36:34 -0800

    experimental: light ev_io binding for net.Server

--- src/node_io_watcher.cc
+++ src/node_io_watcher.cc
@@ -5,6 +5,10 @@
 #include <v8.h>
 
 #include <assert.h>
+#include <stdint.h>  // intptr_t
+#include <stdlib.h>
+
+extern char etext;
 
 namespace node {
 
@@ -12,6 +16,119 @@ using namespace v8;
 
 Persistent<FunctionTemplate> IOWatcher::constructor_template;
 Persistent<String> callback_symbol;
+Persistent<String> on_io_symbol;
+
+
+static inline Local<Integer> AddressToJS(void* address) {
+  intptr_t diff = (intptr_t)address - (intptr_t)&etext;
+  assert(diff > 0 && diff < 0x7FFFFFFF);
+  return Integer::New(diff);
+}
+
+
+template <class T>
+static inline T* JSToAddress(Local<Value> v) {
+  return static_cast<T*>((void*)(v->IntegerValue() + &etext));
+}
+
+
+static void IOCallback(EV_P_ ev_io *watcher, int revents) {
+  HandleScope scope;
+
+  Persistent<Object> obj = *static_cast<Persistent<Object>*>(watcher->data);
+
+  Local<Value> callback_v = obj->Get(on_io_symbol);
+  if (!callback_v->IsFunction()) return;
+
+  Local<Function> callback = Local<Function>::Cast(callback_v);
+
+  TryCatch try_catch;
+
+  Local<Value> argv[4];
+  argv[0] = AddressToJS(watcher);
+  argv[1] = Local<Value>::New(revents & EV_READ ? True() : False());
+  argv[2] = Local<Value>::New(revents & EV_WRITE ? True() : False());
+
+  callback->Call(obj, 3, argv);
+
+  if (try_catch.HasCaught()) {
+    FatalException(try_catch);
+  }
+}
+
+
+static Handle<Value> IOAlloc(const Arguments& args) {
+  HandleScope scope;
+
+  ev_io *io = new ev_io;
+  ev_init(io, IOCallback);
+
+  assert(args[0]->IsObject());
+
+  Persistent<Object> *obj_p = new Persistent<Object>();
+  *obj_p = Persistent<Object>::New(args[0]->ToObject());
+  io->data = obj_p;
+
+  return scope.Close(AddressToJS(io));
+}
+
+
+static Handle<Value> IOFree(const Arguments& args) {
+  HandleScope scope;
+  ev_io *io = JSToAddress<ev_io>(args[0]);
+  delete io;
+  return Undefined();
+}
+
+
+static Handle<Value> IOSet(const Arguments& args) {
+  HandleScope scope;
+
+  ev_io *io = JSToAddress<ev_io>(args[0]);
+
+  if (!args[1]->IsInt32()) {
+    return ThrowException(Exception::TypeError(
+          String::New("First arg should be a file descriptor.")));
+  }
+
+  int fd = args[1]->Int32Value();
+
+  if (!args[2]->IsBoolean()) {
+    return ThrowException(Exception::TypeError(
+          String::New("Second arg should boolean (readable).")));
+  }
+
+  int events = 0;
+
+  if (args[2]->IsTrue()) events |= EV_READ;
+
+  if (!args[3]->IsBoolean()) {
+    return ThrowException(Exception::TypeError(
+          String::New("Third arg should boolean (writable).")));
+  }
+
+  if (args[3]->IsTrue()) events |= EV_WRITE;
+
+  ev_io_set(io, fd, events);
+
+  return Undefined();
+}
+
+
+static Handle<Value> IOStart(const Arguments& args) {
+  HandleScope scope;
+  ev_io *io = JSToAddress<ev_io>(args[0]);
+  ev_io_start(EV_DEFAULT_UC_ io);
+  return Undefined();
+}
+
+
+static Handle<Value> IOStop(const Arguments& args) {
+  HandleScope scope;
+  ev_io *io = JSToAddress<ev_io>(args[0]);
+  ev_io_stop(EV_DEFAULT_UC_ io);
+  return Undefined();
+}
 
 
 void IOWatcher::Initialize(Handle<Object> target) {
@@ -28,7 +145,16 @@ void IOWatcher::Initialize(Handle<Object> target) {
 
   target->Set(String::NewSymbol("IOWatcher"), constructor_template->GetFunction());
 
+  // New style IOWatchers don't use an ObjectWrap, but rather return a raw
+  // pointer in javascript to the ev_io.
+  NODE_SET_METHOD(target, "ioAlloc", IOAlloc);
+  NODE_SET_METHOD(target, "ioSet", IOSet);
+  NODE_SET_METHOD(target, "ioStop", IOStop);
+  NODE_SET_METHOD(target, "ioStart", IOStart);
+  NODE_SET_METHOD(target, "ioFree", IOFree);
+
   callback_symbol = NODE_PSYMBOL("callback");
+  on_io_symbol = NODE_PSYMBOL("_onIO");
 }
 
 
