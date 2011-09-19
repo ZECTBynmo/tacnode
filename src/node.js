f6c4a06d0fac8de0fb8397dba200e7349c7347d5
Ryan Dahl <ry@tinyclouds.org>
2011-09-18 21:32:51 -0700

    Fix test-domains2.js

--- src/node.js
+++ src/node.js
@@ -543,7 +543,11 @@
     domain.enter();
 
     try {
-      obj[method](arg0, arg1, arg2, arg3);
+      if (typeof obj[method] == 'function') {
+        obj[method](arg0, arg1, arg2, arg3);
+      } else {
+        // print error?
+      }
     } catch (e) {
       throw e;
       domain.emit('error', e);
