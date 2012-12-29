452ca3b1eb07a9591f53241628bd13a2538207be
ZECTBynmo <michael.vegeto@gmail.com>
2012-12-28 19:39:59 -0500

    Push V8 libraries into the final linked node.lib

--- tools/concatlibs.py
+++ tools/concatlibs.py
@@ -4,6 +4,8 @@ import sys
 currentPath = os.getcwd()
 
 if sys.platform == 'win32':
-	os.system('lib.exe /OUT:'+currentPath+'/Release/node.lib  '+currentPath+'/Release/lib/cares.lib '+currentPath+'/Release/lib/http_parser.lib '+currentPath+'/Release/lib/libuv.lib '+currentPath+'/Release/lib/node.lib '+currentPath+'/Release/lib/openssl.lib '+currentPath+'/Release/lib/zlib.lib')
+	os.system('lib.exe /OUT:'+currentPath+'/Release/node.lib  '+currentPath+'/Release/lib/cares.lib '+currentPath+'/Release/lib/http_parser.lib '\
+			  +currentPath+'/Release/lib/libuv.lib '+currentPath+'/Release/lib/node.lib '+currentPath+'/Release/lib/openssl.lib '+currentPath+'/Release/lib/zlib.lib '\
+			  +currentPath+'/build/Release/lib/v8_base.lib '+currentPath+'/build/Release/lib/v8_nosnapshot.lib '+currentPath+'/build/Release/lib/v8_snapshot.lib ')
 else:
 	os.system('ar rcs '+currentPath+'/Release/node.a '+currentPath+'/Release/lib/cares.o '+currentPath+'/Release/lib/http_parser.o '+currentPath+'/Release/lib/libuv.o '+currentPath+'/Release/lib/node.o '+currentPath+'/Release/lib/openssl.o '+currentPath+'/Release/lib/zlib.o')
